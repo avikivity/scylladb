4991a1e6a257c96404a5277c903ae4b3c27095cd
Avi Kivity <avi@scylladb.com>
2017-09-04 19:01:35 +0300

    database, sstables, compaction: convert use of thread_scheduling_group to seastar cpu scheduler
    thread_scheduling_groups are converted to plain scheduling_group. Due to
    differences in initialization (scheduling_group initializtion defers), we
    create the scheduling_groups in main.cc and propagate them to users via
    a new class database_config.
    
    Since shares are in the 1-1000 range vs. 0-1 for thread scheduling quotas,
    the flush controller was adjusted to return values within the higher ranges.

--- database.hh
+++ database.hh
@@ -297,8 +297,8 @@ public:
         restricted_mutation_reader_config read_concurrency_config;
         restricted_mutation_reader_config streaming_read_concurrency_config;
         ::cf_stats* cf_stats = nullptr;
-        seastar::thread_scheduling_group* background_writer_scheduling_group = nullptr;
-        seastar::thread_scheduling_group* memtable_scheduling_group = nullptr;
+        seastar::scheduling_group background_writer_scheduling_group = {};
+        seastar::scheduling_group memtable_scheduling_group = {};
         bool enable_metrics_reporting = false;
     };
     struct no_commitlog {};
@@ -741,7 +741,7 @@ public:
         return _config.cf_stats;
     }
 
-    seastar::thread_scheduling_group* background_writer_scheduling_group() {
+    seastar::scheduling_group background_writer_scheduling_group() {
         return _config.background_writer_scheduling_group;
     }
 
@@ -934,8 +934,8 @@ public:
         restricted_mutation_reader_config read_concurrency_config;
         restricted_mutation_reader_config streaming_read_concurrency_config;
         ::cf_stats* cf_stats = nullptr;
-        seastar::thread_scheduling_group* background_writer_scheduling_group = nullptr;
-        seastar::thread_scheduling_group* memtable_scheduling_group = nullptr;
+        seastar::scheduling_group background_writer_scheduling_group = {};
+        seastar::scheduling_group memtable_scheduling_group = {};
         bool enable_metrics_reporting = false;
     };
 private:
@@ -1008,6 +1008,12 @@ public:
     no_such_column_family(const sstring& ks_name, const sstring& cf_name);
 };
 
+
+struct database_config {
+    seastar::scheduling_group background_writer_scheduling_group;
+    seastar::scheduling_group memtable_scheduling_group;
+};
+
 // Policy for distributed<database>:
 //   broadcast metadata writes
 //   local metadata reads
@@ -1043,7 +1049,7 @@ private:
     dirty_memory_manager _dirty_memory_manager;
     dirty_memory_manager _streaming_dirty_memory_manager;
 
-    seastar::thread_scheduling_group _background_writer_scheduling_group;
+    seastar::scheduling_group _background_writer_scheduling_group;
     flush_cpu_controller _memtable_cpu_controller;
 
     semaphore _read_concurrency_sem{max_concurrent_reads()};
@@ -1053,6 +1059,8 @@ private:
     restricted_mutation_reader_config _system_read_concurrency_config;
 
     semaphore _sstable_load_concurrency_sem{max_concurrent_sstable_loads()};
+
+    database_config _dbcfg;
 
     std::unordered_map<sstring, keyspace> _keyspaces;
     std::unordered_map<utils::UUID, lw_shared_ptr<column_family>> _column_families;
@@ -1098,7 +1106,7 @@ public:
 
     future<> parse_system_tables(distributed<service::storage_proxy>&);
     database();
-    database(const db::config&);
+    database(const db::config&, database_config dbcfg);
     database(database&&) = delete;
     ~database();
 
