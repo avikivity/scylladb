483f9c0ed8f75999db62e78e968092b9310428c3
Avi Kivity <avi@scylladb.com>
2017-12-03 13:00:37 +0200

    delayed_tasks: de-inline
    Nothing sensitive here, so can reduce compile time.

--- delayed_tasks.cc
+++ delayed_tasks.cc
@@ -20,5 +20,44 @@
  */
 
 #include "delayed_tasks.hh"
+#include <seastar/core/reactor.hh>  // seastar's timer.hh is not self-contained
+
+delayed_tasks::waiter::waiter(clock::duration d) : _timer([this] { _done.set_value(); }) {
+    _timer.arm(d);
+}
+
+delayed_tasks::waiter::~waiter() {
+    if (_timer.armed()) {
+        _timer.cancel();
+        _done.set_exception(cancelled());
+    }
+}
+
+future<> delayed_tasks::waiter::get_future() noexcept {
+    return _done.get_future();
+}
+
+void delayed_tasks::schedule_after(clock::duration d, noncopyable_function<future<> ()> f) {
+    _logger.trace("Adding scheduled task.");
+
+    auto iter = _waiters.insert(_waiters.end(), std::make_unique<waiter>(d));
+    auto& w = *iter;
+
+    w->get_future().then([this, f = std::move(f)] () mutable {
+        _logger.trace("Running scheduled task.");
+        return f();
+    }).then([this, iter] {
+         // We'll only get here if the instance is still alive, since otherwise the future will be resolved to
+         // `cancelled`.
+        _waiters.erase(iter);
+    }).handle_exception_type([](const cancelled&) {
+        // Nothing.
+        return make_ready_future<>();
+    });
+}
+
+void delayed_tasks::cancel_all() {
+    _waiters.clear();
+}
 
 logging::logger delayed_tasks::_logger("delayed_tasks");
--- delayed_tasks.hh
+++ delayed_tasks.hh
@@ -44,64 +44,29 @@ private:
     static logging::logger _logger;
 
     // A waiter is destroyed before the timer has elapsed.
-    class cancelled : public std::exception {
-    public:
-    };
+    class cancelled : public std::exception {};
 
     class waiter final {
         timer<clock> _timer;
-
         promise<> _done{};
-
     public:
-        explicit waiter(clock::duration d) : _timer([this] { _done.set_value(); }) {
-            _timer.arm(d);
-        }
-
-        ~waiter() {
-            if (_timer.armed()) {
-                _timer.cancel();
-                _done.set_exception(cancelled());
-            }
-        }
-
-        future<> get_future() noexcept {
-            return _done.get_future();
-        }
+        explicit waiter(clock::duration d);
+        ~waiter();
+        future<> get_future() noexcept;
     };
 
     // `std::list` because iterators are not invalidated. We assume that look-up time is not a bottleneck.
     std::list<std::unique_ptr<waiter>> _waiters{};
-
 public:
     //
     // Schedule the task `f` after d` has elapsed. If the instance goes out of scope before
     // the duration has elapsed, then the task is cancelled.
     //
-    void schedule_after(clock::duration d, noncopyable_function<future<> ()> f) {
-        _logger.trace("Adding scheduled task.");
-
-        auto iter = _waiters.insert(_waiters.end(), std::make_unique<waiter>(d));
-        auto& w = *iter;
-
-        w->get_future().then([this, f = std::move(f)] () mutable {
-            _logger.trace("Running scheduled task.");
-            return f();
-        }).then([this, iter] {
-             // We'll only get here if the instance is still alive, since otherwise the future will be resolved to
-             // `cancelled`.
-            _waiters.erase(iter);
-        }).handle_exception_type([](const cancelled&) {
-            // Nothing.
-            return make_ready_future<>();
-        });
-    }
+    void schedule_after(clock::duration d, noncopyable_function<future<> ()> f);
 
     //
     // Cancel all scheduled tasks.
     //
-    void cancel_all() {
-        _waiters.clear();
-    }
+    void cancel_all();
 };
 
