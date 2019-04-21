759a19e3857ac4ebe142172f43d659da5c864f2b
Avi Kivity <avi@scylladb.com>
2019-04-21 15:36:41 +0300

    config: auto-add named_values into config_file
    By passing a config_file into named_value, we remove another call to the
    _make_config_values() macro.

--- utils/config_file.hh
+++ utils/config_file.hh
@@ -109,11 +109,13 @@ public:
         typedef T type;
         typedef named_value<T> MyType;
 
-        named_value(std::string_view name, std::string_view type_name, value_status vs, const T& t = T(), std::string_view desc = {})
+        named_value(config_file* file, std::string_view name, std::string_view type_name, value_status vs, const T& t = T(), std::string_view desc = {})
             : config_src(name, type_name, desc)
             , _value(t)
             , _value_status(vs)
-        {}
+        {
+            file->add(*this);
+        }
         value_status status() const override {
             return _value_status;
         }
