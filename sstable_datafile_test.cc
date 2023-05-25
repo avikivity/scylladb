191de0bf4a8f4b8c06bf0a5bd436a984264df691
Avi Kivity <avi@scylladb.com>
2023-05-25 15:45:29 +0300

    deinclude

--- sstable_datafile_test.cc
+++ sstable_datafile_test.cc
@@ -21,6 +21,10 @@
 #include <ratio>
 #include <type_traits>
 #include <fmt/chrono.h>
+#include <list>
+#include <deque>
+#include <set>
+#include <unordered_set>
 
 // the database clock follows Java - 1ms granularity, 64-bit counter, 1970 epoch
 
@@ -1170,8 +1174,6 @@ struct i_filter {
 };
 }
 
-#include "bytes.hh"
-
 /**
  * This is a very fast, non-cryptographic hash suitable for general hash-based
  * lookup. See http://murmurhash.googlepages.com/ (Murmur Hash 2) and
@@ -1299,6 +1301,120 @@ void hash3_x64_128(bytes_view key, uint64_t seed, std::array<uint64_t, 2>& resul
 } // namespace murmur_hash
 
 } // namespace utils
+
+
+#include <boost/range/adaptor/transformed.hpp>
+
+namespace utils {
+
+template <std::ranges::range Range>
+std::ostream& format_range(std::ostream& os, const Range& items, std::string_view paren = "{}") {
+    fmt::print(os, "{}{}{}", paren.front(), fmt::join(items, ", "), paren.back());
+    return os;
+}
+
+namespace internal {
+
+template<bool NeedsComma, typename Printable>
+struct print_with_comma {
+    const Printable& v;
+};
+
+template<bool NeedsComma, typename Printable>
+std::ostream& operator<<(std::ostream& os, const print_with_comma<NeedsComma, Printable>& x) {
+    os << x.v;
+    if (NeedsComma) {
+        os << ", ";
+    }
+    return os;
+}
+
+} // namespace internal
+} // namespace utils
+
+namespace std {
+
+template <typename K, typename V>
+std::ostream& operator<<(std::ostream& os, const std::pair<K, V>& p) {
+    os << "{" << p.first << ", " << p.second << "}";
+    return os;
+}
+
+template<typename... T, size_t... I>
+std::ostream& print_tuple(std::ostream& os, const std::tuple<T...>& p, std::index_sequence<I...>) {
+    return ((os << "{" ) << ... << utils::internal::print_with_comma<I < sizeof...(I) - 1, T>{std::get<I>(p)}) << "}";
+}
+
+template <typename... T>
+std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& p) {
+    return print_tuple(os, p, std::make_index_sequence<sizeof...(T)>());
+}
+
+// Vector-like ranges
+template <std::ranges::range Range>
+requires (
+       std::same_as<Range, std::vector<std::ranges::range_value_t<Range>>>
+    || std::same_as<Range, std::list<std::ranges::range_value_t<Range>>>
+    || std::same_as<Range, std::initializer_list<std::ranges::range_value_t<Range>>>
+    || std::same_as<Range, std::deque<std::ranges::range_value_t<Range>>>
+)
+std::ostream& operator<<(std::ostream& os, const Range& items) {
+    return utils::format_range(os, items);
+}
+
+template <typename T, typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::set<T, Args...>& items) {
+    return utils::format_range(os, items);
+}
+
+template <typename T, typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::unordered_set<T, Args...>& items) {
+    return utils::format_range(os, items);
+}
+
+template <typename K, typename V, typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::map<K, V, Args...>& items) {
+    return utils::format_range(os, items);
+}
+
+template <typename... Args>
+std::ostream& operator<<(std::ostream& os, const boost::transformed_range<Args...>& items) {
+    return utils::format_range(os, items);
+}
+
+template <typename T, std::size_t N>
+std::ostream& operator<<(std::ostream& os, const std::array<T, N>& items) {
+    return utils::format_range(os, items, "[]");
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::optional<T>& opt) {
+    if (opt) {
+        os << "{" << *opt << "}";
+    } else {
+        os << "{}";
+    }
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::strong_ordering& order);
+std::ostream& operator<<(std::ostream& os, const std::weak_ordering& order);
+std::ostream& operator<<(std::ostream& os, const std::partial_ordering& order);
+
+} // namespace std
+
+template<typename T>
+struct fmt::formatter<std::optional<T>> : fmt::formatter<std::string_view> {
+    template <typename FormatContext>
+    auto format(const std::optional<T>& opt, FormatContext& ctx) const {
+        if (opt) {
+            return fmt::format_to(ctx.out(), "{}", *opt);
+        } else {
+            return fmt::format_to(ctx.out(), "{{}}");
+        }
+     }
+};
+
 
 #include "utils/large_bitset.hh"
 
